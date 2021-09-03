d7687ae70904438886b051e127db862c974b9dd8
Zeapherine <zeapherine@gmail.com>
2021-09-03 21:44:54 +0530

    added topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+import './topbar.css';
 
 const Topbar = () => {
-    return (
-        <div>
-            topbar
-        </div>
-    )
-}
+	return (
+		<div className='topbar'>
+			<div className='topbarWrapper'>
+				<div className='topLeft'>
+					<span className='logo'>lamaadmin</span>
+				</div>
+				<div className='topRight'>
+					<div className='topbarIconContainer'>
+						<NotificationsNone />
+						<span className='topIconBadge'>2</span>
+					</div>
+
+					<div className='topbarIconContainer'>
+						<Language />
+						<span className='topIconBadge'>2</span>
+					</div>
+
+					<div className='topbarIconContainer'>
+						<Settings />
+					</div>
+
+					<img
+						src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'
+						alt='profile'
+						className='topAvatar'
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Topbar;
